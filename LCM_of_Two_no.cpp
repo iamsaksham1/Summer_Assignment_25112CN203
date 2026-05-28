eb3818ea79907625053bccc264c76ded91134afb
iamsakshamrathore@gmail.com <iamsakshamrathore@gmai.com>
2026-05-28 21:17:56 +0530

    this gives the LCM of two given numbers

---
+++ LCM_of_Two_no.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+int main(){
+    using namespace std;
+    int a,b,LCM;
+    cout<<"Enter the first number :";
+    cin>>a;
+    cout<<"Enter the first number :";
+    cin>>b;
+    for(int i=1; i<=a && i<=b ; i++){
+        
+    }
+}
