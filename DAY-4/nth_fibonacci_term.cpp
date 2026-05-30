3fd55d5a4c4a37bcadeffc2fb5d8afae045fcae4
iamsakshamrathore@gmail.com <iamsakshamrathore@gmai.com>
2026-05-30 21:17:20 +0530

    REFACTORING

---
+++ DAY-4/nth_fibonacci_term.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+int main(){
+    using namespace std;
+    int n; 
+    cout<<" Enter the number :";
+    cin>>n;
+    int first=0;
+    int second=1;
+    int next;
+    for(int i =1; i<=n; i++){
+        next=first + second;
+        cout<<first<<" ";
+        first=second;
+        second=next;
+    }
+    return 0;
+}
