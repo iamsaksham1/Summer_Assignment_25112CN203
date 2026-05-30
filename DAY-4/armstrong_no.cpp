e290639976f9515b3d073db04d08e422b202b59b
iamsakshamrathore@gmail.com <iamsakshamrathore@gmai.com>
2026-05-30 23:05:44 +0530

    this checks for armstrong number

--- DAY-4/armstrong_no.cpp
+++ DAY-4/armstrong_no.cpp
@@ -1,3 +1,21 @@
 #include <iostream>
 using namespace std;
-int main()
+int main(){
+   int n;
+   int digits, sum=0, old;
+    cout<<" enter the number :";
+    cin>>n;
+    old = n;
+    while(n>0){
+        digits = n%10;
+        sum = sum + (digits*digits*digits);
+        n=n/10;
+    }
+    if ( sum == old){
+        cout<<" it is an armstrong number"<<endl;
+    }
+    else {
+        cout<<" it is not an armstrong number"<<endl;}
+        return 0;
+
+}
