5562776e6df6cbf7e9cdba349026ca6e8639af0d
iamsakshamrathore@gmail.com <iamsakshamrathore@gmail.com>
2026-05-31 18:35:01 +0530

    This gives the largest prime factor of a number

--- DAY-5/largest_prime_factor.cpp
+++ DAY-5/largest_prime_factor.cpp
@@ -4,7 +4,19 @@ int main(){
     int n;
     cout<<" enter the number :";
     cin>>n;
-    for( int i=1 ; i<=n ; i++){
-        if()
+    int largest = -1;
+    while(n%2 == 0){
+       largest = 2;
+       n=n/2;
     }
-}
+    for( int i=3 ; i*i<=n ; i+=2){
+        while( n%i ==0 ){
+           largest =i;
+            n=n/i;
+        }
+        }
+        if(n>1){
+            cout<<" the largest prime factor is :"<<n<<endl;
+        }
+        return 0;
+   }
