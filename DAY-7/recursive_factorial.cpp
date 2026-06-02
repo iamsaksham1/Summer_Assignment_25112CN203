952f2dc29b01cf5ce6ac5b6ec6c77dbb41693647
iamsakshamrathore@gmail.com <iamsakshamrathore@gmail.com>
2026-06-02 17:52:44 +0530

    DAY-7 Question No-1 added

---
+++ DAY-7/recursive_factorial.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int factorial( int n){
+    if( n<=1){
+        return 1;
+    }
+    return  n * factorial(n-1);
+}
+int main(){
+    int x;
+    cout<<" Enter the number :"<<endl;
+    cin>>x;
+    cout<<" the factorial of "<<x<<" is "<<factorial(x)<<endl;
+    return 0;
+}
