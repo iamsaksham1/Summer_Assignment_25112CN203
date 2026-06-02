de859cfae7182afe1587c27763d02700002780c9
iamsakshamrathore@gmail.com <iamsakshamrathore@gmail.com>
2026-06-02 17:57:54 +0530

    DAY-7 Question No-1 added

--- DAY-7/recursive_factorial.cpp
+++ DAY-7/recursive_factorial.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 using namespace std;
-int factorial( int n){
+long long factorial( int n){
     if( n<=1){
         return 1;
     }
@@ -10,6 +10,10 @@ int main(){
     int x;
     cout<<" Enter the number :"<<endl;
     cin>>x;
+    if(x < 0){
+        cout << "Factorial is not defined for negative numbers.";
+        return 0;
+    }
     cout<<" the factorial of "<<x<<" is "<<factorial(x)<<endl;
     return 0;
 }
