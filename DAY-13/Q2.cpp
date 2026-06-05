1708129c4c8491d7b8e391269e0e0282d0264d9b
iamsakshamrathore@gmail.com <iamsakshamrathore@gmail.com>
2026-06-05 22:30:00 +0530

    DAY-13 Question-2 added

--- DAY-13/Q2.cpp
+++ DAY-13/Q2.cpp
@@ -8,11 +8,13 @@ int main(){
     int sum=0;
     double avg;
     cout << "Enter the elements:\n";
+    
     for(int i=0; i<l; i++){
         cin>>arr[i];
         sum+=arr[i];
         avg= double(sum)/l;
     }
+
     cout<<"the sum of digits of array is : "<<sum<<endl;
     cout<<"the average of digits of array is : "<<avg<<endl;
     return 0;
